05f600be0e126711f83c349465932135656124df
Jeff Meyer <jcmeyer10@gmail.com>
2017-07-08 19:54:57 -0400

    adds functions to add fish to state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -5,6 +5,27 @@ import Inventory from './Inventory';
 import Order from './Order';
 
 class App extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {
+      fishes: {},
+      order: {}
+    }
+    this.addFish = this.addFish.bind(this);
+  }
+
+  addFish(fish){
+    // update our state by taking copy of current state then updating
+    const fishes = {...this.state.fishes};
+    // add in new fish
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    // set state
+    // fishes: fishes is same as fishes in es6
+    this.setState({ fishes })
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -12,7 +33,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     )
   }
